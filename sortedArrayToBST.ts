6e4e1267edd578cf8079d759efbf00fba90b3026
regalhotpocket <regalhotpocket@users.noreply.github.com>
2022-03-12 12:56:59 -0500

    Create sortedArrayToBST.ts

---
+++ sortedArrayToBST.ts
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+// Solution accepted on 03/12/2022 12:49
+// Runtime: 68 ms, faster than 99.45% of TypeScript online submissions
+// Memory Usage: 45.3 MB, less than 41.99% of TypeScript online submissions
+function sortedArrayToBST(nums: number[]): TreeNode | null {
+    function helper(left: number, right: number): TreeNode | null {
+        if (left >= right)
+            return null;
+        const middle = left + ~~((right - left) / 2);
+        return new TreeNode(nums[middle],
+                            helper(left, middle),
+                            helper(middle + 1, right));
+    };
+    return helper(0, nums.length);
+};
