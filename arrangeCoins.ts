45715d10170077ecc858b6a8d5d0781efcb6166a
regalhotpocket <regalhotpocket@users.noreply.github.com>
2022-03-12 18:54:19 -0500

    Create arrangeCoins.ts

---
+++ arrangeCoins.ts
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/arranging-coins/submissions/
+// Solution accepted at 03/12/2022 18:45
+// Runtime: 107 ms, faster than 92.86% of TypeScript online submissions.
+// Memory Usage: 44.9 MB, less than 46.43% of TypeScript online submissions.
+// NOTE: What is the runtime on this? Seems bounded by O(n), but it seems to
+//       be something like a reverse factorial decay. Don't know how to analyze.
+function arrangeCoins(n: number): number {
+    for (var r = 2, c = 1; c <= n; c += r, r += 1);
+    return r - 2;
+};
