066bfab727c1843afe243fb98fc31fdfce721242
regalhotpocket <regalhotpocket@users.noreply.github.com>
2022-03-14 10:53:56 -0400

    Create isValid.ts

---
+++ TypeScript/isValid.ts
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/valid-parentheses/submissions/
+// 03/14/2022 10:52
+// Runtime: 125 ms, faster than 22.48% of TypeScript online submissions for Valid Parentheses.
+// Memory Usage: 45 MB, less than 19.21% of TypeScript online submissions for Valid Parentheses.
+function isValid(s: string): boolean {
+    const stack = [];
+    for (const c of s) {
+        if (['(','{','['].includes(c))
+            stack.push(c);
+        else if  (((c == ')' && stack[stack.length - 1] == '(')) ||
+                  ((c == '}' && stack[stack.length - 1] == '{')) ||
+                  ((c == ']' && stack[stack.length - 1] == '[')))
+            stack.pop();
+        else
+            return false;
+    }
+    return stack.length == 0;
+};
