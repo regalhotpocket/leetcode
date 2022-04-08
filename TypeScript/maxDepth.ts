bec90eff656f3bc2ea13d698399f71b56d52277b
regalhotpocket <regalhotpocket@users.noreply.github.com>
2022-04-07 21:49:56 -0400

    Create maxDepth.ts

---
+++ TypeScript/maxDepth.ts
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/
+// 04/07/2022 21:48
+// Runtime: 123 ms, faster than 25.51% of TypeScript online submissions for Maximum Depth of Binary Tree.
+// Memory Usage: 46.6 MB, less than 39.89% of TypeScript online submissions for Maximum Depth of Binary Tree.
+function maxDepth(root: TreeNode | null): number {
+    return root ? 1 + Math.max(maxDepth(root.left), maxDepth(root.right)) : 0; 
+};
