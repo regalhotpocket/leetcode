f060902c5e04242bc651fe2e239009a0f53b77ce
regalhotpocket <regalhotpocket@users.noreply.github.com>
2022-04-14 14:31:08 -0700

    Create 2235.java

---
+++ Java/2235.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/add-two-integers/
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Add Two Integers.
+// Memory Usage: 39.6 MB, less than 75.58% of Java online submissions for Add Two Integers.
+// 04/14/2022 14:28
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
