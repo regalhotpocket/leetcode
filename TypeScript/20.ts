76715a90a631eb39bec21a03c9da696a4d0d34b0
regalhotpocket <regalhotpocket@users.noreply.github.com>
2022-05-05 20:20:40 -0400

    Update and rename isValid.ts to 20.ts

--- TypeScript/20.ts
+++ TypeScript/20.ts
@@ -1,7 +1,7 @@
-// https://leetcode.com/problems/valid-parentheses/submissions/
-// 03/14/2022 10:52
+// https://leetcode.com/problems/valid-parentheses/
 // Runtime: 125 ms, faster than 22.48% of TypeScript online submissions for Valid Parentheses.
 // Memory Usage: 45 MB, less than 19.21% of TypeScript online submissions for Valid Parentheses.
+// 03/14/2022 10:52
 function isValid(s: string): boolean {
     const stack = [];
     for (const c of s) {
