5229f5879364cba0908808507701da61e38a6584
regalhotpocket <regalhotpocket@users.noreply.github.com>
2022-05-05 20:10:44 -0400

    Update and rename twoSum.ts to 2.ts

--- TypeScript/2.ts
+++ TypeScript/2.ts
@@ -1,7 +1,7 @@
-// https://leetcode.com/problems/two-sum/submissions/
-// 03/14/2022 10:38
+// https://leetcode.com/problems/two-sum/
 // Runtime: 88 ms, faster than 84.33% of TypeScript online submissions for Two Sum.
 // Memory Usage: 45.3 MB, less than 30.34% of TypeScript online submissions for Two Sum.
+// 03/14/2022 10:38
 function twoSum(nums: number[], target: number): number[] {
     const found = new Map<number, number>();
     for (const [i, v] of nums.entries()) {
