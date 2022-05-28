92f74a45eb1c18a02a6df9797f60ac1366229bd7
regalhotpocket <regalhotpocket@users.noreply.github.com>
2022-05-28 00:32:14 -0400

    Update and rename arrangeCoins.ts to 441.ts

--- TypeScript/441.ts
+++ TypeScript/441.ts
@@ -1,7 +1,7 @@
-// https://leetcode.com/problems/arranging-coins/submissions/
-// Solution accepted at 03/12/2022 18:45
+// https://leetcode.com/problems/arranging-coins/
 // Runtime: 107 ms, faster than 92.86% of TypeScript online submissions.
 // Memory Usage: 44.9 MB, less than 46.43% of TypeScript online submissions.
+// Solution accepted at 03/12/2022 18:45
 // NOTE: What is the runtime on this? Seems bounded by O(n), but it seems to
 //       be something like a reverse factorial decay. Don't know how to analyze.
 function arrangeCoins(n: number): number {
