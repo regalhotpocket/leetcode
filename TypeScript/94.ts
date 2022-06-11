8d520246bfeb81e1b9dd9e88d77447dbdbc2cead
regalhotpocket <regalhotpocket@users.noreply.github.com>
2022-06-10 23:25:18 -0400

    Update and rename inorderTraversal.ts to 94.ts

--- TypeScript/94.ts
+++ TypeScript/94.ts
@@ -1,7 +1,7 @@
-// https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/
-// 03/20/2022 14:56
+// https://leetcode.com/problems/binary-tree-inorder-traversal/
 // Runtime: 77 ms, faster than 74.92% of TypeScript online submissions for Binary Tree Inorder Traversal.
 // Memory Usage: 44.4 MB, less than 51.92% of TypeScript online submissions for Binary Tree Inorder Traversal.
+// 03/20/2022 14:56
 function inorderTraversal(root: TreeNode | null): number[] {
     var result = [];
     function inorder(node: TreeNode | null): void {
